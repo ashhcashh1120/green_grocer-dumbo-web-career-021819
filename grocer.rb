9abec25505fb7fd46c99c6259842d07e1608a546
Ashley Archibald <aarchibald20@gmail.com>
2019-02-19 04:36:19 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -12,4 +12,19 @@ end
 
 def checkout(cart, coupons)
   # code here
-end
+  def checkout(cart: [], coupons: [])
+  # code here	  cart = consolidate_cart(cart: cart)
+end 	
+   cart = apply_coupons(cart: cart, coupons: coupons)
+  cart = apply_clearance(cart: cart)
+  total = 0
+  cart.each do |item_name, item_data|
+    total += (item_data[:price] * item_data[:count])
+  end
+
+   if total > 100
+    0.9 * total
+  else
+    total
+  end
+  end
